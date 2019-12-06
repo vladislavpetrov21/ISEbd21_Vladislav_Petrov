73862ea94c73b78a5edb16b02878ea48e357de24
vladislavpetrov21 <21vlapet@gmail.com>
2019-12-06 21:49:10 +0400

    laba1klassn

---
+++ TP/TP/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace TP
+{
+    partial class FormFly
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormFly));
+            this.pictureBoxFly = new System.Windows.Forms.PictureBox();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFly)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxFly
+            // 
+            this.pictureBoxFly.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxFly.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxFly.Name = "pictureBoxFly";
+            this.pictureBoxFly.Size = new System.Drawing.Size(884, 461);
+            this.pictureBoxFly.TabIndex = 0;
+            this.pictureBoxFly.TabStop = false;
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonUp.BackgroundImage")));
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonUp.Location = new System.Drawing.Point(690, 302);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(56, 53);
+            this.buttonUp.TabIndex = 1;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLeft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonLeft.BackgroundImage")));
+            this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLeft.Location = new System.Drawing.Point(627, 357);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(57, 52);
+            this.buttonLeft.TabIndex = 2;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDown.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonDown.BackgroundImage")));
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDown.Location = new System.Drawing.Point(690, 357);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(56, 52);
+            this.buttonDown.TabIndex = 3;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonRight.BackgroundImage")));
+            this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRight.Location = new System.Drawing.Point(752, 357);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(54, 52);
+            this.buttonRight.TabIndex = 4;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(38, 27);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(69, 55);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Создать";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // FormFly
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.pictureBoxFly);
+            this.Name = "FormFly";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Самолет";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFly)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxFly;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ TP/TP/Sturmovic.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP
+{
+    class Sturmovic
+    {
+        /// <summary>
+        /// Левая координата отрисовки самолета
+        /// </summary>
+        private float _startPosX;
+        /// <summary>
+        /// Правая кооридната отрисовки самолета
+        /// </summary>
+        private float _startPosY;
+        /// <summary>
+        /// Ширина окна отрисовки
+        /// </summary>
+        private int _pictureWidth;
+        /// <summary>
+        /// Высота окна отрисовки
+        /// </summary>
+        private int _pictureHeight;
+        /// <summary>
+        /// Ширина отрисовки самолета
+        /// </summary>
+        private const int carWidth = 100;
+        /// <summary>
+        /// Ширина отрисовки самолета
+        /// </summary>
+        private const int carHeight = 60;
+        /// <summary>
+        /// Максимальная скорость
+        /// </summary>
+        public int MaxSpeed { private set; get; }
+        /// <summary>
+        /// Вес самолета
+        /// </summary>
+        public float Weight { private set; get; }
+        /// <summary>
+        /// Основной цвет кузова
+        /// </summary>
+        public Color MainColor { private set; get; }
+        /// <summary>
+        /// Дополнительный цвет
+        /// </summary>
+        public Color DopColor { private set; get; }
+        /// <summary>
+        /// Признак наличия звезды
+        /// </summary>
+        public bool Star { private set; get; }
+        /// <summary>
+        /// Признак наличия бомб
+        /// </summary>
+        public bool Bomb { private set; get; }
+        /// <summary>
+        /// Признак наличия ракет
+        /// </summary>
+        public bool Rocket { private set; get; }
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="maxSpeed">Максимальная скорость</param>
+        /// <param name="weight">Вес автомобиля</param>
+        /// <param name="mainColor">Основной цвет кузова</param>
+        /// <param name="dopColor">Дополнительный цвет</param>
+        /// <param name="star">Признак наличия звезды</param>
+        /// <param name="bomb">Признак наличия бомб</param>
+        /// <param name="rocket">Признак наличия ракет</param>
+        public Sturmovic(int maxSpeed, float weight, Color mainColor, Color dopColor,
+       bool star, bool bomb, bool rocket)
+        {
+            MaxSpeed = maxSpeed;
+            Weight = weight;
+            MainColor = mainColor;
+            DopColor = dopColor;
+            Star = star;
+            Bomb = bomb;
+            Rocket = rocket;
+        }
+        /// <summary>
+        /// Установка позиции cамолета
+        /// </summary>
+        /// <param name="x">Координата X</param>
+        /// <param name="y">Координата Y</param>
+        /// <param name="width">Ширина картинки</param>
+        /// <param name="height">Высота картинки</param>
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            _startPosX = x;
+            _startPosY = y;
+            _pictureWidth = width;
+            _pictureHeight = height;
+        }
+        /// <summary>
+        /// Изменение направления пермещения
+        /// </summary>
+        /// <param name="direction">Направление</param>
+        public void MoveTransport(Direction direction)
+        {
+            float step = MaxSpeed * 100 / Weight;
+            switch (direction)
+            {
+                // вправо
+                case Direction.Right:
+                    if (_startPosX + step < _pictureWidth - carWidth)
+                    {
+                        _startPosX += step;
+                    }
+                    break;
+                //влево
+                case Direction.Left:
+                    if (_startPosX - step > 0)
+                    {
+                        _startPosX -= step;
+                    }
+                    break;
+                //вверх
+                case Direction.Up:
+                    if (_startPosY - step > 0)
+                    {
+                        _startPosY -= step;
+                    }
+                    break;
+                //вниз
+                case Direction.Down:
+                    if (_startPosY + step < _pictureHeight - carHeight)
+                    {
+                        _startPosY += step;
+                    }
+                    break;
+            }
+        }
+        /// <summary>
+        /// Отрисовка самолета
+        /// </summary>
+        /// <param name="g"></param>
+        public void DrawFly(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+            //рисуем звезду
+            if (Star)
+            {
+                Point point1 = new Point((int)_startPosX + 85, (int)_startPosY + 60);
+                Point point2 = new Point((int)_startPosX + 90, (int)_startPosY + 65);
+                Point point3 = new Point((int)_startPosX + 95, (int)_startPosY + 60);
+                Point point4 = new Point((int)_startPosX + 95, (int)_startPosY + 90);
+                Point point5 = new Point((int)_startPosX + 90, (int)_startPosY + 85);
+                Point point6 = new Point((int)_startPosX + 85, (int)_startPosY + 90);
+                Point[] board = { point1, point2, point3, point4, point5, point6 };
+                g.DrawPolygon(pen, board);
+                Point point7 = new Point((int)_startPosX + 85, (int)_startPosY + 10);
+                Point point8 = new Point((int)_startPosX + 90, (int)_startPosY + 15);
+                Point point9 = new Point((int)_startPosX + 95, (int)_startPosY + 10);
+                Point point10 = new Point((int)_startPosX + 95, (int)_startPosY + 40);
+                Point point11 = new Point((int)_startPosX + 90, (int)_startPosY + 35);
+                Point point12 = new Point((int)_startPosX + 85, (int)_startPosY + 40);
+                Point[] board1 = { point7, point8, point9, point10, point11, point12 };
+                g.DrawPolygon(pen, board1);
+            }
+            //рисуем бомбы
+            if (Bomb)
+            {
+                g.DrawEllipse(pen, _startPosX + 90, _startPosY, 30, 10);
+                Brush spoiler = new SolidBrush(DopColor);
+                g.FillEllipse(spoiler, _startPosX + 90, _startPosY, 30, 10);
+                g.DrawEllipse(pen, _startPosX + 90, _startPosY + 90, 30, 10);
+                g.FillEllipse(spoiler, _startPosX + 90, _startPosY + 90, 30, 10);
+            }
+            // теперь отрисуем фюзеляж самолета
+            //границы самолета
+            g.DrawRectangle(pen, _startPosX, _startPosY + 40, 130, 20);
+            g.DrawRectangle(pen, _startPosX + 75, _startPosY - 10, 25, 120);
+            Brush brRed = new SolidBrush(MainColor);
+            g.FillRectangle(brRed, _startPosX, _startPosY + 40, 130, 20);
+            Brush brGreen = new SolidBrush(MainColor);
+            g.FillRectangle(brGreen, _startPosX + 75, _startPosY - 10, 25, 120);
+            g.DrawRectangle(pen, _startPosX + 5, _startPosY + 25, 15, 50);
+            g.FillRectangle(brGreen, _startPosX + 5, _startPosY + 25, 15, 50);
+            Brush brBrown = new SolidBrush(MainColor);
+            g.FillEllipse(brBrown, _startPosX + 130, _startPosY + 35, 10, 30);
+            g.DrawEllipse(pen, _startPosX + 130, _startPosY + 35, 10, 30);
+            g.FillEllipse(brBrown, _startPosX + 95, _startPosY, 10, 30);
+            g.DrawEllipse(pen, _startPosX + 95, _startPosY, 10, 30);
+            g.FillEllipse(brBrown, _startPosX + 95, _startPosY + 70, 10, 30);
+            g.DrawEllipse(pen, _startPosX + 95, _startPosY + 70, 10, 30);
+            Brush brBlue = new SolidBrush(MainColor);
+            g.FillEllipse(brBlue, _startPosX + 70, _startPosY + 42, 30, 15);
+            g.DrawEllipse(pen, _startPosX + 70, _startPosY + 42, 30, 15);
+            // рисуем ракеты
+            if (Rocket)
+            {
+                g.DrawRectangle(pen, _startPosX + 80, _startPosY + 10, 40, 10);
+                Brush spoiler = new SolidBrush(DopColor);
+                g.FillRectangle(spoiler, _startPosX + 80, _startPosY + 10, 40, 10);
+                g.DrawRectangle(pen, _startPosX + 80, _startPosY + 80, 40, 10);
+                g.FillRectangle(spoiler, _startPosX + 80, _startPosY + 80, 40, 10);
+            }
+        }
+    }
+}
